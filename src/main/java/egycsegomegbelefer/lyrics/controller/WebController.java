c6f53673e409ac9997c59df3bf241aeaf22bc89f
Darrnel <redzsiby14@gmail.com>
2018-12-01 21:05:34 +0100

    - Security config - Login/reg controller - app properties modified with SQL properties
    Signed-off-by: Darrnel <redzsiby14@gmail.com>

--- src/main/java/egycsegomegbelefer/lyrics/controller/WebController.java
+++ src/main/java/egycsegomegbelefer/lyrics/controller/WebController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WebController {
 
-    @GetMapping("/")
+    @GetMapping("/asd")
     public String indexLoader(){
         return "index";
     }
