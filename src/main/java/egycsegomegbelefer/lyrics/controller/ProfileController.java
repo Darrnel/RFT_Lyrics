66c91430185ef87522c96d54c6ad6104306b1893
holydins <holydins10@gmail.com>
2018-12-07 21:07:58 +0100

    Profile show data about user from backend.

--- src/main/java/egycsegomegbelefer/lyrics/controller/ProfileController.java
+++ src/main/java/egycsegomegbelefer/lyrics/controller/ProfileController.java
@@ -1,7 +1,11 @@
 package egycsegomegbelefer.lyrics.controller;
 
+import egycsegomegbelefer.lyrics.domain.Rang;
+import egycsegomegbelefer.lyrics.domain.User;
 import egycsegomegbelefer.lyrics.service.UserService;
+import org.springframework.security.core.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,6 +20,11 @@ public class ProfileController {
     @RequestMapping(value="/profile", method = RequestMethod.GET)
     public ModelAndView index() {
         ModelAndView modelAndView = new ModelAndView();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        User user = userService.findUserByEmail(auth.getName());
+        user.setRang(Rang.UJONC);
+        modelAndView.addObject("userName", user.getUserName());
+        modelAndView.addObject("rang", user.getRang().toString().toLowerCase());
         modelAndView.setViewName("profile");
         return modelAndView;
     }
