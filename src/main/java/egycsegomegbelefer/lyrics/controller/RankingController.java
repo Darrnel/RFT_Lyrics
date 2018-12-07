28407194f6948be9079532e7561197f9ba179a8e
holydins <holydins10@gmail.com>
2018-12-07 20:25:29 +0100

    Create ranking html + css + controller.

--- src/main/java/egycsegomegbelefer/lyrics/controller/RankingController.java
+++ src/main/java/egycsegomegbelefer/lyrics/controller/RankingController.java
@@ -1,22 +1,17 @@
 package egycsegomegbelefer.lyrics.controller;
 
-import egycsegomegbelefer.lyrics.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class ProfileController {
+public class RankingController {
 
-    @Autowired
-    private UserService userService;
-
-    @RequestMapping(value="/profile", method = RequestMethod.GET)
+    @RequestMapping(value="/ranking", method = RequestMethod.GET)
     public ModelAndView index() {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("profile");
+        modelAndView.setViewName("ranking");
         return modelAndView;
     }
 }
