7ee93165aac6331a2676702d142fc7d9d5382834
darrnel <redzsiby@gmail.com>
2018-12-12 14:08:52 +0100

    - CSS bugfix

--- src/main/java/egycsegomegbelefer/lyrics/repository/LyricsRepository.java
+++ src/main/java/egycsegomegbelefer/lyrics/repository/LyricsRepository.java
@@ -5,7 +5,6 @@ import egycsegomegbelefer.lyrics.domain.Lyrics;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import sun.awt.SunHints;
 
 import java.util.List;
 @Repository("lyricsRepository")
